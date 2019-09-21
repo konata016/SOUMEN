c56037035651853b6b6a43d2b36207b840546c7c
Unknown <konata016@gmail.com>
2019-09-22 01:28:28 +0900

    落下ができていない

--- SQUARE_ENIX_INTERN/Assets/My/Script/PlMove.cs
+++ SQUARE_ENIX_INTERN/Assets/My/Script/PlMove.cs
@@ -6,45 +6,62 @@ using System;
 
 public class PlMove : MonoBehaviour
 {
-    public float speed = 10;
+    public float speed = 20;
+    public float high = 100;
 
-    struct PlayerParameter
+    public struct PlayerParameter
     {
         public float speed;
+        public bool isJump;
+        public float high;
     }
-    PlayerParameter plParam = new PlayerParameter();
+    public static PlayerParameter plParam = new PlayerParameter();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        plParam.high = high;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Parameter(Time.deltaTime);
+
         var roll = transform.rotation.eulerAngles;
+        Debug.Log(roll.z);
 
         //回転
-        if (roll.z < 90 && roll.z > -90)
+        if (roll.z < 90 || roll.z > 270)
         {
-            roll.z += speed * MyJoyCon.joyconDec.accel.y;
-            transform.rotation = Quaternion.Euler(roll);
+            //roll.z += speed * MyJoyCon.joyconDec.accel.y;
+            roll.z += plParam.speed * DebugMode();
         }
-        if(roll.z > 90)
+        else
         {
-            Debug.Log("aaa");
+            plParam.isJump = true;
         }
-        if(roll.z < -90)
-        {
-            Debug.Log("bbb");
-        }
+        
 
-
+        transform.rotation = Quaternion.Euler(roll);
     }
 
     void Parameter(float timer)
     {
         plParam.speed = speed * timer;
-    }   
+
+    }
+
+    float DebugMode()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            return -speed / 2;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            return speed / 2;
+        }
+        return 0;
+    }
 }
