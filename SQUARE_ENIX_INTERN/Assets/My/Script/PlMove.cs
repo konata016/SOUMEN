0487b6f000f347b9e8716cf98273b860560d990f
konata016 <konata016@gmail.com>
2019-09-21 20:59:28 +0900

    更新

--- SQUARE_ENIX_INTERN/Assets/My/Script/PlMove.cs
+++ SQUARE_ENIX_INTERN/Assets/My/Script/PlMove.cs
@@ -8,28 +8,6 @@ public class PlMove : MonoBehaviour
 {
     public float speed = 10;
 
-
-    private static readonly Joycon.Button[] m_buttons =
-         Enum.GetValues(typeof(Joycon.Button)) as Joycon.Button[];
-
-    private List<Joycon> m_joycons;
-    private Joycon m_joyconL;
-    private Joycon m_joyconR;
-    private Joycon.Button? m_pressedButtonL;
-    private Joycon.Button? m_pressedButtonR;
-
-    struct JoyConDec
-    {
-        public bool isLeft;
-        public Joycon.Button button;
-        public float[] stick;
-        public Vector3 gyro;
-        public Vector3 accel;
-        public Quaternion orientation;
-
-    }
-    JoyConDec joyconDec = new JoyConDec();
-
     struct PlayerParameter
     {
         public float speed;
@@ -46,26 +24,27 @@ public class PlMove : MonoBehaviour
     void Update()
     {
         var roll = transform.rotation.eulerAngles;
-        roll.z += speed * joyconDec.accel.y;
 
-        transform.rotation = Quaternion.Euler(roll);
+        //回転
+        if (roll.z < 90 && roll.z > -90)
+        {
+            roll.z += speed * MyJoyCon.joyconDec.accel.y;
+            transform.rotation = Quaternion.Euler(roll);
+        }
+        if(roll.z > 90)
+        {
+            Debug.Log("aaa");
+        }
+        if(roll.z < -90)
+        {
+            Debug.Log("bbb");
+        }
+
+
     }
 
     void Parameter(float timer)
     {
         plParam.speed = speed * timer;
-    }
-
-    void GetJoyCon()
-    {
-
-        //後できれいにする
-
-        joyconDec.isLeft = m_joycons[1].isLeft;
-        //joyconDec.button = joyconDec.isLeft ? m_pressedButtonL : m_pressedButtonR;
-        joyconDec.stick = m_joycons[1].GetStick();
-        joyconDec.gyro = m_joycons[1].GetGyro();
-        joyconDec.accel = m_joycons[1].GetAccel();
-        joyconDec.orientation = m_joycons[1].GetVector();
-    }
+    }   
 }
