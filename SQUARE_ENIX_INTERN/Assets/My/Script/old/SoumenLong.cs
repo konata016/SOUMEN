6bd07e70ca820b94e2c2fd10f2b029c64d22ee97
konata016 <konata016@gmail.com>
2019-09-24 04:42:09 +0900

    素麺モデル完成

--- SQUARE_ENIX_INTERN/Assets/My/Script/old/SoumenLong.cs
+++ SQUARE_ENIX_INTERN/Assets/My/Script/old/SoumenLong.cs
@@ -5,7 +5,9 @@ using Obi;
 
 public class SoumenLong : MonoBehaviour
 {
+    public GameObject coreObj;
     int soumenSiz;
+    Vector3 startSiz;
 
 
     public List<GameObject> obiObj = new List<GameObject>();
@@ -13,6 +15,8 @@ public class SoumenLong : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        startSiz = coreObj.transform.localScale;
+
         //入っているプレハブを調べる
         Debug.Log(transform.GetChildCount());
         for (int i = 0; i < transform.GetChildCount(); i++)
@@ -29,18 +33,22 @@ public class SoumenLong : MonoBehaviour
 
     void SoumenSizChange()
     {
-        for (int i=0; i < transform.GetChildCount(); i++) {
 
-            switch (Score.scoreRank)
-            {
-                case Score.RANK.Ryou: soumenSiz = 1; break;
-                case Score.RANK.Yuu: soumenSiz = 2; break;
-                case Score.RANK.Syuu: soumenSiz = 3; break;
-                default: break;
-            }
+        switch (Score.scoreRank)
+        {
+            case Score.RANK.Ryou: soumenSiz = 1; break;
+            case Score.RANK.Yuu: soumenSiz = 2; break;
+            case Score.RANK.Syuu: soumenSiz = 3; break;
+            default: break;
+        }
+
+        coreObj.transform.localScale = startSiz + new Vector3((float)soumenSiz * 1.5f, (float)soumenSiz * 1.5f, (float)soumenSiz * 1.5f);
+
+        for (int i=0; i < transform.GetChildCount(); i++) {
 
             //obi.GetComponent<ObiDistanceConstraints>().stretchingScale = soumenSiz;
             obiObj[i].GetComponent<ObiRopeCursor>().ChangeLength((float)soumenSiz * 1.5f);
         }
+
     }
 }
