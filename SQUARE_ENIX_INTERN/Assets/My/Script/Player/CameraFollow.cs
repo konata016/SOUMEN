6949816dad1edad44e33649d5e2bf9ab1d701127
Kanau Shibayama <kabori257@gmail.com>
2019-09-24 09:33:20 +0900

    ステージ完成。その他マイナーチェンジ。

--- SQUARE_ENIX_INTERN/Assets/My/Script/Player/CameraFollow.cs
+++ SQUARE_ENIX_INTERN/Assets/My/Script/Player/CameraFollow.cs
@@ -13,17 +13,22 @@ public class CameraFollow : MonoBehaviour
     /* --- 変数 ---*/
 
     /* --- private変数 --- */
+    private PlayerController playerController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = player.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 3);
+        if (!playerController.isJump)
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 3);
+            transform.rotation = Quaternion.Euler(defaultCameraAngle);
+        }
     }
 
     public void setDefaultRotate()
