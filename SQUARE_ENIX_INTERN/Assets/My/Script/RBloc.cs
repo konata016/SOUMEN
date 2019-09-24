0b636d0a893f0cf5ad7e6fb0a3d0e5bd7f1faf67
konata016 <konata016@gmail.com>
2019-09-24 11:46:23 +0900

    バグ修正

---
+++ SQUARE_ENIX_INTERN/Assets/My/Script/RBloc.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RBloc : MonoBehaviour
+{
+
+    public GameObject plCore;
+    public static bool on { get; set; }
+    public GameObject pl;
+
+    Vector3 tmpPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var roll = plCore.transform.rotation.eulerAngles;
+
+        if (tmpPos == Vector3.zero || tmpPos == null)
+        {
+            tmpPos = pl.transform.position;
+        }
+
+        if (roll.z < 255f && roll.z > 180f)
+        {
+            roll.z = 280f;
+        }
+        plCore.transform.rotation = Quaternion.Euler(roll);
+    }
+}
